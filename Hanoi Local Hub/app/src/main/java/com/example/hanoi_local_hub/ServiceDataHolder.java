62a6b1da866509d09e0b8ce77653432a5335ac5b
Quỳnh Anh <nguyenquynhanh2682k3@gmail.com>
2025-06-13 10:10:21 +0700

    upload

---
+++ Hanoi Local Hub/app/src/main/java/com/example/hanoi_local_hub/ServiceDataHolder.java
@@ -0,0 +1,9 @@
+package com.example.hanoi_local_hub;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiceDataHolder {
+    // Dữ liệu dịch vụ dùng chung toàn app
+    public static List<ServiceItem> allServices = new ArrayList<>();
+}
