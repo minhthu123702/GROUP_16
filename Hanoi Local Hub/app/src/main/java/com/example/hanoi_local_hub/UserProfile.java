882d0bc15da2bc216b07abe30e299a1c0fac1f94
Quỳnh Anh <nguyenquynhanh2682k3@gmail.com>
2025-06-22 20:56:38 +0700

    upload

--- Hanoi Local Hub/app/src/main/java/com/example/hanoi_local_hub/UserProfile.java
+++ Hanoi Local Hub/app/src/main/java/com/example/hanoi_local_hub/UserProfile.java
@@ -1,26 +1,26 @@
 package com.example.hanoi_local_hub;
 
 public class UserProfile {
-    public String userId;
-    public String username;
+    public String uid;
+    public String avatarResId;
+    public String name;
     public String cccd;
     public String email;
     public String phone;
     public String birthday;
-    public String fullName;
-    public String gender; // "Nam" hoặc "Nữ"
+    public String gender;
 
-    public UserProfile() {} // Bắt buộc cho Firebase
+    public UserProfile() {}
 
-    public UserProfile(String userId, String username, String cccd, String email,
-                       String phone, String birthday, String fullName, String gender) {
-        this.userId = userId;
-        this.username = username;
+    public UserProfile(String uid, String avatarResId, String name, String cccd, String email,
+                       String phone, String birthday, String gender) {
+        this.uid = uid;
+        this.avatarResId = avatarResId;
+        this.name = name;
         this.cccd = cccd;
         this.email = email;
         this.phone = phone;
         this.birthday = birthday;
-        this.fullName = fullName;
         this.gender = gender;
     }
 }
